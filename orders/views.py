f464fdaa0d33ad87bea93a9e16aaeb641e65f5a8
officiallyjo <josie9447@gmail.com>
2025-06-29 09:33:27 -0500

    paypal payment intergration

--- orders/views.py
+++ orders/views.py
@@ -2,11 +2,32 @@ from django.shortcuts import render, redirect
 from carts.models import CartItem
 from .forms import OrderForm
 import datetime
-from .models import Order
-# Create your views here.
+from .models import Order,Payment
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+
 
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user , is_ordered=False, order_number =body['orderID'] )
+    
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status']
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+
     return render(request,'orders/payments.html')
 
 def place_order(request , total=0, quantity=0):
